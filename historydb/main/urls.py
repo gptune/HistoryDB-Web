5ade94f67870382d30a6918c17703dc26f97fa0c
Younghyun Cho <yhcho614@gmail.com>
2020-12-08 20:56:57 +0900

    Added basic project structure and main index page

---
+++ historydb/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls import include
+
+from . import views
+
+from django.conf.urls import url
+from django.contrib import admin
+
+app_name = "main"
+urlpatterns = [
+    path('', views.index, name='index'),
+]
