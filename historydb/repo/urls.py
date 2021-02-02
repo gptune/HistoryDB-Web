d2f6456aa58f13c75f05393ee551d6adcc33415e
Younghyun Cho <yhcho614@gmail.com>
2021-02-01 17:50:39 -0800

    Remove unused codes/pages

--- historydb/repo/urls.py
+++ historydb/repo/urls.py
@@ -5,15 +5,12 @@ from . import views
 
 app_name = "repo"
 urlpatterns = [
-    path('base/', views.base, name='base'),
     path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
     path('userdashboard/', views.UserDashboard.as_view(), name='userdashboard'),
     path('entryaccess/', views.EntryAccess.as_view(), name='entryaccess'),
     path('entrydel/', views.EntryDel.as_view(), name='entrydel'),
-    path('examples/', views.Examples.as_view(), name='examples'),
     path('upload/', views.Upload.as_view(), name='upload'),
     path('addapp/', views.AddApp.as_view(), name='addapp'),
-    path('return/', views.base, name='return'),
     path('export/', views.Export.as_view(), name='export'),
     path('archive/<str:perf_data_uid>/', views.query, name='query'),
 ]
