810bf238a294a8096c57dc55718b4332a662e38c
Younghyun Cho <yhcho614@gmail.com>
2021-02-25 10:11:07 -0800

    Updated Web Reset Script

--- web-reset.py
+++ web-reset.py
@@ -4,3 +4,20 @@ import json
 with open("./historydb/django_keys.json", "w") as f_out:
     data = {"SECRET_KEY":str(get_random_secret_key())}
     f_out.write(json.dumps(data, indent=2))
+
+def read_from_file(file_path):
+    with open(file_path, "r") as f_in:
+        return f_in.read()
+
+with open("./historydb/email_settings.json", "w") as f_out:
+    data = {}
+    data["EMAIL_BACKEND"] = read_from_file("/secrets/email-backend")
+    data["EMAIL_USE_TLS"] = read_from_file("/secrets/email_use_tls")
+    data["EMAIL_PORT"] = read_from_file("/secrets/email_port")
+    data["EMAIL_HOST"] = read_from_file("/secrets/email_host")
+    data["EMAIL_HOST_USER"] = read_from_file("/secrets/email_host_user")
+    data["EMAIL_HOST_PASSWORD"] = read_from_file("/secrets/email-password")
+    data["SERVER_EMAIL"] = read_from_file("/secrets/server_email")
+
+    import json
+    f_out.write(json.dumps(data))
