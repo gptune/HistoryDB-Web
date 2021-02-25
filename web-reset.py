85362c1612e377ad924f9cd1e78233d211a94ece
Younghyun Cho <yhcho614@gmail.com>
2021-02-25 10:17:12 -0800

    Updated Web Reset Script

--- web-reset.py
+++ web-reset.py
@@ -12,12 +12,12 @@ def read_from_file(file_path):
 with open("./historydb/email_settings.json", "w") as f_out:
     data = {}
     data["EMAIL_BACKEND"] = read_from_file("/secrets/email-backend")
-    data["EMAIL_USE_TLS"] = read_from_file("/secrets/email_use_tls")
-    data["EMAIL_PORT"] = read_from_file("/secrets/email_port")
-    data["EMAIL_HOST"] = read_from_file("/secrets/email_host")
-    data["EMAIL_HOST_USER"] = read_from_file("/secrets/email_host_user")
-    data["EMAIL_HOST_PASSWORD"] = read_from_file("/secrets/email-password")
-    data["SERVER_EMAIL"] = read_from_file("/secrets/server_email")
+    data["EMAIL_USE_TLS"] = read_from_file("/secrets/email-use-tls")
+    data["EMAIL_PORT"] = read_from_file("/secrets/email-port")
+    data["EMAIL_HOST"] = read_from_file("/secrets/email-host")
+    data["EMAIL_HOST_USER"] = read_from_file("/secrets/email-host-user")
+    data["EMAIL_HOST_PASSWORD"] = read_from_file("/secrets/email-host-password")
+    data["SERVER_EMAIL"] = read_from_file("/secrets/server-email")
 
     import json
     f_out.write(json.dumps(data))
