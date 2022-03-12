71cf6e8a0e7ea557890549d3b92849d920386e05
Younghyun Cho <yhcho614@gmail.com>
2022-03-11 22:46:27 -0800

    Minor fix

--- historydb/repo/views.py
+++ historydb/repo/views.py
@@ -805,7 +805,7 @@ class ModelPrediction(TemplateView):
                 output_info = tuning_problem_info["output_info"][i]
                 output_name = output_info["output_name"]
                 output_type = output_info["output_type"]
-                if output_type == "integer" opr output_type == "real":
+                if output_type == "integer" or output_type == "real":
                     problem = {
                         "name": output_name,
                         "type": output_type,
@@ -1526,7 +1526,7 @@ class SADashboard(TemplateView):
                 output_name = output_info["output_name"]
                 output_type = output_info["output_type"]
 
-                if output_type == "integer" opr output_type == "real":
+                if output_type == "integer" or output_type == "real":
                     problem = {
                         "name": output_name,
                         "type": output_type,
